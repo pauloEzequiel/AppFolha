5091478499e5fb9319889217829faf570c5f609a
pauloEzequiel <paulo.ezequiel@souunit.com.br>
2022-10-29 00:32:18 -0300

    Adição de base do projeto

---
+++ appfolha/src/main/java/br/edu/infnet/appfolha/AppfolhaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appfolha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfolhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfolhaApplication.class, args);
+	}
+
+}
